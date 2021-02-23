2e067f93b4715138fca3bf42aebb1e042d597d99
Alex Kiura <kiuraalex@gmail.com>
2021-02-23 10:59:46 +0300

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useRef, useEffect, createContext, useContext, useReducer } from 'react';
+import React, { useReducer, useRef, useEffect } from 'react';
+import { createContainer } from 'react-tracked';
 
 const useFlasher = () => {
   const ref = useRef(null);
@@ -8,17 +9,15 @@ const useFlasher = () => {
       ref.current.classList.remove('flash');
     }, 300);
   });
-  return ref
+  return ref;
 };
 
 const initialState = {
   count1: 0,
   count2: 0,
-  count3: 0
-}
+};
 
-
-const reducer = (state = initialState, action) => {
+const reducer = (state, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return {
@@ -31,50 +30,38 @@ const reducer = (state = initialState, action) => {
         [action.name]: state[action.name] - 1,
       };
     default:
-      return state
+      return state;
   }
-}
+};
 
 const useValue = () => useReducer(reducer, initialState);
 
-const Context = createContext(null);
-
-const useGlobalState = () => {
-  const value = useContext(Context);
-  if (value == null) throw new Error('Please add GlobalStateProvider');
-  return value;
-}
-
-const GlobalStateProvider = ({ children }) => (
-  <Context.Provider value={useValue()}>{children}</Context.Provider>
-);
+const {
+  useTracked: useGlobalState,
+  Provider: GlobalStateProvider,
+} = createContainer(useValue);
 
 const Counter = ({ name }) => {
   const [state, dispatch] = useGlobalState();
-
   return (
     <div ref={useFlasher()}>
       {state[name]}
-      <button onClick={() => dispatch({ type: 'INCREMENT', name})}>+1</button>
-      <button onClick={() => dispatch({ type: 'DECREMENT', name})}>-1</button>
+      <button onClick={() => dispatch({ type: 'INCREMENT', name })}>+1</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT', name })}>-1</button>
     </div>
   );
-
 };
-
 
 const App = () => (
   <GlobalStateProvider>
     <h1>Count1</h1>
-    <Counter name="count1"/>
-    <Counter name="count1"/>
+    <Counter name="count1" />
+    <Counter name="count1" />
     <h1>Count2</h1>
-    <Counter name="count2"/>
-    <Counter name="count2"/>
-    <h1>Count3</h1>
-    <Counter name="count3"/>
-    <Counter name="count3"/>
+    <Counter name="count2" />
+    <Counter name="count2" />
   </GlobalStateProvider>
 );
 
 export default App;
+
